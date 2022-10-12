becda2a52972ebd5fd47bd15a6855872d24420a7
rzepka2u <thomas.rzepka8@etu.univ-lorraine.fr>
2022-10-12 22:10:22 +0200

    Ajout de la dependance JUnit au fichier pom.xml Ajout du dossier pour les tests et ajout d'un premier squelette de test (TestJoueur.java)

---
+++ Daedalus/src/test/java/TestJoueur.java
@@ -0,0 +1,39 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Classe de test des fonctions du joueur
+ */
+public class TestJoueur {
+
+    /**
+     * Test verifiant qu'un joueur ne peut pas se deplacer lorsqu'un mur le bloque
+     */
+    @Test
+    public void test_collisionMur() {
+        // preparation des donnees
+
+        // methodes testées
+
+        // verifications des donnees
+        assertEquals("Ceci devrait être comme cela", 1, 1);
+
+    }
+
+
+    /**
+     * Test verifiant qu'un joueur peut se deplacer lorsqu'il n'y a pas de mur pour le bloquer
+     */
+    @Test
+    public void test_nonCollisionMur() {
+        // preparation des donnees
+
+        // methodes testées
+
+        // verifications des donnees
+        assertEquals("Ceci devrait être comme cela", 1, 1);
+
+    }
+
+}

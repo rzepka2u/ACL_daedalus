52ef83764b14bbfcfeeed37882f6e35ca3d034f6
Victor NOEL <victor.noel2@etu.univ-lorraine.fr>
2022-11-06 16:02:30 +0100

    Modification de Arme, ajout commentaires, getters et constructeur par défaut

--- Daedalus/src/main/java/model/objets/Arme.java
+++ Daedalus/src/main/java/model/objets/Arme.java
@@ -4,11 +4,22 @@ import model.enums.ZoneAttaque;
 
 public class Arme implements Tresor {
 
+    // le nom de l'arme
     private String nom;
+    // les dégâts qu'elle inflige
     private int degats;
+    // la portée de son attaque
     private int portee;
+    // la zone effective de son attaque
     private ZoneAttaque zone;
 
+    /**
+     * Constructeur avec paramètres de l'arme
+     * @param nom de l'arme
+     * @param degats de l'arme
+     * @param portee de l'arme
+     * @param zone d'attaque de l'arme
+     */
     public Arme(String nom, int degats, int portee, ZoneAttaque zone){
         this.nom = nom;
         this.degats = degats;
@@ -16,8 +27,46 @@ public class Arme implements Tresor {
         this.zone = zone;
     }
 
+    /**
+     * Constructeur vide d'une arme qui construit une arme de base
+     */
+    public Arme() {
+        this.nom = "Epée en bois";
+        this.degats = 1;
+        this.portee = 1;
+        this.zone = ZoneAttaque.CASE_DEVANT;
+    }
+
+    /**
+     * Renvoie le nom de l'arme
+     * @return l'attribut nom
+     */
     public String getNom(){
         return nom;
+    }
+
+    /**
+     * Renvoie les dégâts de l'arme
+     * @return l'attribut degats
+     */
+    public int getDegats() {
+        return this.degats;
+    }
+
+    /**
+     * Renvoie la portée de l'arme
+     * @return l'attribut portee
+     */
+    public int getPortee() {
+        return this.portee;
+    }
+
+    /**
+     * Renvoie la zone d'attaque de l'arme
+     * @return l'attribut zone
+     */
+    public ZoneAttaque getZone() {
+        return zone;
     }
 
 }

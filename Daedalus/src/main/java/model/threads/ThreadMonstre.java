c102f86481057ba84dd5504870aedd3f5e9065f4
poirel41u <jeremy.poirel9@etu.univ-lorraine.fr>
2022-11-08 12:11:16 +0100

    Correction threadMonstre

--- Daedalus/src/main/java/model/threads/ThreadMonstre.java
+++ Daedalus/src/main/java/model/threads/ThreadMonstre.java
@@ -2,9 +2,8 @@ package model.threads;
 
 import model.objets.Entite;
 import model.objets.Fantome;
-import model.objets.Gobelin;
 import model.objets.Jeu;
-import model.objets.Labyrinthe;
+
 
 import java.lang.Thread;
 import java.util.Random;
@@ -20,46 +19,56 @@ public class ThreadMonstre extends Thread {
         positionInList = pos;
     }
 
-    /*public void run(){
-        int px,py;
-        px = 0;
-        py = 0;
-        Entite m =  jeu.getEntites().get(positionInList+1);
-        Random r = new Random();
-        int random = r.nextInt(4);
-        random = random+1;
-        switch(random) {
-            case 1:
-                px = 1;
-                py = 0;
-            break;
-            case 2:
-                px = 0;
-                py = 1;
-            break;
-            case 3:
-                px = -1;
-                py = 0;
-            break;
-            case 4:
-                px = 0;
-                py = -1;
-            break;
-        }
+    public void run(){
 
-        if(m instanceof Fantome) {
-            m.seDeplacer(m.getX() + px, m.getY() + py);
-        } else {
-            if(jeu.validerDeplacement(m.getX() + px, m.getY() + py)) {
-                m.seDeplacer(m.getX() + px, m.getY() + py);
+        int px,py,random;
+        Entite m;
+        Random r = new Random();
+        int hauteur = jeu.getLabyrinthe().getHauteur();
+        int largeur = jeu.getLabyrinthe().getLargeur();
+
+        while(true){
+
+            px = 0;
+            py = 0;
+            m =  jeu.getEntites().get(positionInList+1);
+            random = r.nextInt(4);
+            random = random+1;
+
+            switch(random) {
+                case 1:
+                    px = 1;
+                    py = 0;
+                break;
+                case 2:
+                    px = 0;
+                    py = 1;
+                break;
+                case 3:
+                    px = -1;
+                    py = 0;
+                break;
+                case 4:
+                    px = 0;
+                    py = -1;
+                break;
+            }
+
+            if(m instanceof Fantome) {
+                if(m.getX() >= 0 && m.getY() >= 0 && m.getX() <= hauteur-1 && m.getY() <= largeur-1){
+                    m.seDeplacer(m.getX() + px, m.getY() + py);
+                }
+            } else {
+                if(jeu.validerDeplacement(m.getX() + px, m.getY() + py)) {
+                    m.seDeplacer(m.getX() + px, m.getY() + py);
+                }
+            }
+
+            try{
+                sleep(1000);
+            } catch (InterruptedException e){
+                System.out.println("SLEEP INTERROMPU");
             }
         }
-
-        try{
-            sleep(750);
-        } catch (InterruptedException e){
-            System.out.println("SLEEP INTERROMPU");
-        }
-    }*/
-    
+    }
 }

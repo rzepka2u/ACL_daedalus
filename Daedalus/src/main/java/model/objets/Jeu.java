f0b35ed72f912993de5d352e708d7399b5eae19a
Victor NOEL <victor.noel2@etu.univ-lorraine.fr>
2022-11-08 16:16:43 +0100

    Ajout synchronisation sur l'attaque

--- Daedalus/src/main/java/model/objets/Jeu.java
+++ Daedalus/src/main/java/model/objets/Jeu.java
@@ -378,7 +378,6 @@ public class Jeu{
                     synchronized (this.verrousEntites.get(index)) {
                         ent.prendreDegat(dgts);
                     }
-
                 }
             }
         } else if(cmd.getOrdre() == Ordre.OUVRIR){
